b9da58a554919b86179740f381633723cc420721
HeyTech <mujtaba.aldebes@hotmail.com>
2018-07-08 10:40:35 +0200

    added game files and fixed project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Uno-Multi-Game',
+    version='0.1.0',
+    author='Mujtaba Aldebes, Ranjani Subramanyan, Nandhini Subramanyan',
+    author_email='mujtaba.aldebes@hotmail.com, ransub17@student.hh.se, nansub17@student.hh.se',
+    packages=find_packages(),
+    test_suite='test',
+    tests_require=['nose'],
+)
